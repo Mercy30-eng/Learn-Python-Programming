c10878e7653970b7dd2d107afcf098c0f8351f5d
MercyP30-eng <munzenzimercy20@gmail.com>
2024-03-12 05:51:44 +0000

    Print string in a specific format

---
+++ 0x00Python-Basics/stringsx01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+a = """Twinkle, twinkle, little star,
+            How I wonder what you are!
+                    Up above the world so high,  
+                    Like a diamond in the sky.
+Twinkle, twinkle, little star,
+        How I wonder what you are """
+print(a)
