3bc5b4e645ec343c9534d6751e6403080e6dc8fe
MercyP30-eng <munzenzimercy20@gmail.com>
2024-03-22 04:36:01 +0000

    Prints the documentation of functions

---
+++ 0x00Python-Basics/0x011Doc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Print the docstring (documentation) of the 'abs' function
+print(abs.__doc__)   ##take note of the double underscores
+
+#Print the docstring of the 'split' function
+print(str.split.__doc__)
