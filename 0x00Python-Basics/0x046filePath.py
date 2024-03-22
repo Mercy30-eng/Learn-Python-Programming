640cb9637e30f7a87d8eeb058154dcebafabb528
MercyP30-eng <munzenzimercy20@gmail.com>
2024-03-22 06:27:56 +0000

    Prints path of file being executed

---
+++ 0x00Python-Basics/0x046filePath.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Import the 'os' module to work with the operating system.
+import os
+# Use the 'os.path.realpath(__file__)' to get the full path of the current Python script.
+print("Current File Name: ", os.path.realpath(__file__))        # This will print the path of the current file.
