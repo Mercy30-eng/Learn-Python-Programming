10786fd2625232a20079541ebbd3b378c44a1288
MercyP30-eng <munzenzimercy20@gmail.com>
2024-03-22 05:07:45 +0000

    hero document

---
+++ 0x00Python-Basics/0x13here_doc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#A here document (or "heredoc") is a way of specifying a text block, preserving the line breaks, indents, and other whitespace within the text.
+
+# To do this simply use triple double-quotes to create a multi-line string
+
+print("""
+        a string that you "don't" have to escape
+        This
+        is a ........multiline
+        heredoc string ------->example
+        """)
