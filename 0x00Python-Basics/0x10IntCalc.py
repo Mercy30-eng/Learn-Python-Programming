f12f9851a7c78409467a1dce52ca65e11d21c54d
MercyP30-eng <munzenzimercy20@gmail.com>
2024-03-22 04:10:00 +0000

    prompts user to input an integer and compute it

---
+++ 0x00Python-Basics/0x10IntCalc.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+n = int(input("Enter an integer: ")) # Sample value of n
+result = n + int(str(n) * 2) + int(str(n) * 3)  # Compute the value of n+nn+nnn
+print(result)  # Output the result
